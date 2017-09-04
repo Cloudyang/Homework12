5f41dae14c1035786079ac1499d4f6197fbc0667
yy <yy_yeab@hotmail.com>
2017-09-04 21:07:02 +0800

    mvc

---
+++ WebMvc5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebMvc5.Startup))]
+namespace WebMvc5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
