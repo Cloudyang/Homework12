88d12f9e26577d771c33100a3c38f8e024d13173
yy <yy_yeab@hotmail.com>
2017-09-05 06:00:07 +0800

    待续

---
+++ UnityIOC/UnityResolve.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using Microsoft.Practices.Unity.InterceptionExtension;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace UnityIOC
+{
+    class UnityResolve<T> where T : class
+    {
+        private static T Instance;
+        static UnityResolve()
+        {
+            ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap();
+            fileMap.ExeConfigFilename = Path.Combine(Environment.CurrentDirectory, "CfgFiles", "Unity.Config.xml");
+            Configuration configuration = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+            UnityConfigurationSection section = (UnityConfigurationSection)configuration.GetSection(UnityConfigurationSection.SectionName);
+
+            IUnityContainer container = new UnityContainer();
+            section.Configure(container, "Homework12ContainerAOP");
+            Instance = container.Resolve<T>();
+        }
+        public static T GetInstance()
+        {
+            return Instance;
+        }
+    }
+}
