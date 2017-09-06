3305fb0d2d9b5fab6767456baa577345eab7a234
杨云 <yy_yeab@hotmail.com>
2017-09-06 18:08:33 +0800

    待续

--- UnityIOC.Tests/UnitTest1.cs
+++ UnityIOC.Tests/UnitTest1.cs
@@ -8,11 +8,11 @@ namespace UnityIOC.Tests
     [TestClass]
     public class UnitTest1
     {
-        private ServiceFactory sf = ServiceFactory.Instance;
         [TestMethod]
         public void TestMethod1()
         {
-       //     var container = sf.GetEFService<JDContext>().Container;
+            var sf = ServiceFactory.Instance;
+            Assert.IsNotNull(sf);
         }
     }
 }
