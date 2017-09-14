6f4064b6a55ef8ba974e53fa0e50ca05829f0527
杨云 <yy_yeab@hotmail.com>
2017-09-14 13:01:55 +0800

    待续

---
+++ Bussiness.Service/SearchService.cs
@@ -0,0 +1,37 @@
+﻿using Entity.Model;
+using Bussiness.IService;
+using Service.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussiness.Service
+{
+    public class SearchService : ISearch
+    {
+
+        public PageResult<CommodityModel> QueryCommodityPage(int pageIndex, int pageSize, string keyword, int[] categoryIdList, string priceFilter, string priceOrderBy)
+        {
+            RemoteSearchService.SearcherClient client = null;
+
+            try
+            {
+                client = new RemoteSearchService.SearcherClient();
+                string result = client.QueryCommodityPage(pageIndex, pageSize, keyword, categoryIdList, priceFilter, priceOrderBy);
+
+                client.Close();//会关闭链接，但是如果网络异常了，会抛出异常而且关闭不了
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<PageResult<CommodityModel>>(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                if (client != null)
+                    client.Abort();
+                throw ex;
+            }
+
+        }
+    }
+}
